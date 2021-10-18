8a843cbaaca9c87a9d3251df651fec9af83de93b
siham A <sihamananzh@outlook.sa  git config --global user.name siham>
2021-10-18 13:24:40 +0300

    add-form-search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -17,15 +18,25 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
-    <div className="container max-auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((image) => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>
+    <div className="container mx-auto">
+      <ImageSearch serchText={(text) => setTerm(text)} />
+      {!isloading && images.length === 0 && (
+        <h1 className="text-5xl text-center max-auto mt-32 ">
+          No images found
+        </h1>
+      )}
+      {isloading ? (
+        <h1 className="text-6xl text-center max-auto mt-32 ">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
